aa1a821d9aeb7128aa6de16b134792b1e59718cb
unknown <ederhenriquevicentejust18@gmail.com>
2025-01-11 16:33:16 -0300

    Mensagem do commit

--- public/index.php
+++ public/index.php
@@ -1,25 +1,51 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require_once (dirname(__DIR__) ."\app\config.php");
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
 
-// Incluir o autoload
-require_once dirname(__DIR__)."\app\autoload.php";
+<body>
+    <?php include_once "navbar.php" ?>
+    <?php
 
-// Exemplo básico de roteamento
-$requestUri = str_replace('/public', '', $_SERVER['REQUEST_URI']);
+    require_once(dirname(__DIR__) . "\app\config.php");
 
-switch ($requestUri) {
-    case '/':
-        echo "Página inicial";
-        break;
+    // Incluir o autoload
+    require_once dirname(__DIR__) . "\app\autoload.php";
 
-    case '/tarefas':
-        $controller = new TarefaController();
-        $controller->verTodasAsTarefas();
-        break;
+    // Exemplo básico de roteamento
+    $requestUri = str_replace('/public', '', $_SERVER['REQUEST_URI']);
 
-    default:
-        http_response_code(404);
-        echo "Página não encontrada.";
-        break;
-}
+    switch ($requestUri) {
+        case '/':
+            echo "Página inicial";
+            break;
+
+        case '/tarefas':
+            $controller = new TarefaController();
+            $controller->verTodasAsTarefas();
+            break;
+
+        case '/criar':
+            $controller = new TarefaController();
+            $controller->criarTarefa();
+            break;
+
+        default:
+            http_response_code(404);
+            echo "Página não encontrada.";
+            break;
+    }
+
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
